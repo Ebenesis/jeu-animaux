ffb71035b79d559b5830fd12c9de49806de35aaa
Murielle <beegenxtra@gmail.com>
2021-04-21 17:02:33 +0200

    jeu des animaux

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -72,7 +73,97 @@ export class HomePage {
       playing: false
     }
   ];
+  //stockage de animal choisi
+  public chosenAnimal = null;
 
-  constructor() {}
+  //Le temps maxi pour la reponse
+  private answerDelaySecound = 30;
 
+  //Le temps restant pour une reponse
+  public secoundLeft = null;
+//le chrono pour les reponses
+private timer = null;
+
+
+  //stockage audio html objet qui sait lire un son
+  private audio: HTMLAudioElement = null;
+  constructor(private toastCtrl: ToastController) { }
+
+  public play() {
+    //choisir une image au hasard
+    let alea = Math.floor(Math.random() * this.animalList.length);
+    this.chosenAnimal = this.animalList[alea];
+
+    //arrêter le son précédent(! not this audio ended si c'est pas terminé)
+    if (this.audio && !this.audio.ended) {
+      this.audio.pause();
+    }
+    //lancement du chrono
+    this.startTimer();
+
+    //Jouer un son
+    //instanciation d'un objet audio avec le son que l'on veut jouer
+    this.audio = new Audio('/assets' + this.chosenAnimal.file);
+    //chargement du son
+    this.audio.load();
+    //lecture du son
+    this.audio.play();
+  }
+  public async guessAnimal(clickedAnimal) {
+
+    //si aucun son joue dans ce cas chosenAnimal et null
+    if (!this.chosenAnimal) {
+      return;
+    }
+    //le message à afficher
+    let message;
+    let toastColor = 'danger'
+
+    //comparaison des animaux
+    //celui sur lequel le joueur a cliqué
+    //et ce lui dont on a joué le son
+    //on relie le son à animal
+    if (this.chosenAnimal.title == clickedAnimal.title) {
+      message = 'gagné';
+      toastColor = 'success'
+      this.resetGame();
+    } else {
+      message ='essayé encore';
+    }
+    //Affichage du message
+const toast = await this.toastCtrl.create({
+  message: message,
+  duration: 1000,
+  position: 'top',
+color: toastColor
+});
+
+toast.present();
+  }
+  //Mise à zero du jeu
+  private resetGame(){
+    this.chosenAnimal = null;
+    this.audio = null;
+    //arrete le chrono lorsque la reponse et bonne
+    this.secoundLeft = 0;
+    clearInterval(this.timer);
+
+  }
+private startTimer(){
+  this.secoundLeft = this.answerDelaySecound;
+//On definit le contenu du chronometre
+  this.timer = setInterval(
+    () => {
+      //Décrémenter le temps restant
+      this.secoundLeft--;
+      //s'il reste plus de temps on met le jeu à zero
+      if(this.secoundLeft ==0) {
+        this.resetGame();
+        //annulation du chrono
+        clearInterval(this.timer);
+      }
+    },
+    1000
+  )
+}
 }
